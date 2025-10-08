7f3e31feb3442867e2f165f9d13d0828e8b0d702
Julia Tyulyupa <yul5876@yandex.ru>
2025-10-08 20:56:09 +0300

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,24 @@
+# Импорт настроек из модуля configuration, который содержит параметры конфигурации, такие как URL сервиса
+import configuration
+
+# Импорт библиотеки requests для выполнения HTTP-запросов
+import requests
+
+# Импорт данных запроса из модуля data, в котором определены заголовки и тело запроса
+import data
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_products_kits(products_ids):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+response = post_products_kits(data.products_ids)
+
+# Вывод HTTP-статус кода ответа на запрос
+# Код состояния указывает на результат обработки запроса сервером
+print(response.status_code)
+print(response.json()) 
