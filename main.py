8e59f7b96ca8fb6d1c8455ee13bd708d9599dc9e
Юлия Тюлюпа <yul5876@yandex.ru>
2025-11-02 11:44:35 +0300

    Добавлен финальный скриншот с именем и когортой

---
+++ main.py
@@ -0,0 +1,10 @@
+# Юлия Тюлюпа, 37-я когорта — Финальный проект. Инженер по тестированию плюс
+
+def something_wrong_here(a, b):
+    if a == b:
+        print('Тест пройден')
+    else:
+        print('Тест провален')
+
+
+something_wrong_here('C', 'C')
